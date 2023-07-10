9f7608dda864b65f96805a38dd987d30916fdff5
crystal <crdiaz@csumb.edu>
2023-07-09 20:20:43 -0700

    I added a new student js file with a dialog requires input student name and email also asking my teammate to review my code for my assignment

---
+++ src/components/AddStudent.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import {SERVER_URL} from '../constants.js'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cookies from 'js-cookie';
+
+// properties addStudent red, function called when Add clicked.
+class AddStudent extends Component {
+      constructor(props) {
+	  //initializing component state
+      super(props);
+      this.state = {open: false, name:'', email:'', status_code:0 };
+    };
+	
+    //Functions for when add button is clicked opens the modal form to enter id
+    handleClickOpen = () => {
+      this.setState( {open:true} );
+    };
+	
+	//Function to close modal form
+    handleClose = () => {
+      this.setState( {open:false} );
+    };
+	
+	//Function to handle input field change
+    handleChange = (event) => {
+	//name value entered by user
+      //this.setState({[event.target.name]: event.target.value});
+	  this.setState({student:{name: event.target.value}});
+	   this.setState({student:{email: event.target.value}});
+    }
+
+    //Function to add a student and close modal form
+    handleAdd = () => {
+		//call method when complete and close
+       this.newStudent({name: this.state.name, email:this.state.email});
+       this.handleClose();
+    }
+	
+	 newStudent = (student) => {
+      // API call to add a new student
+      fetch(`${SERVER_URL}/student`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(student)
+      })
+        .then((res) => {
+          if (res.ok) {
+            // Display success toast notification
+            toast.success('A new student added successfully.', {
+              position: toast.POSITION.BOTTOM_LEFT
+            });
+          } else {
+            // Display error toast notification
+            toast.error('Error to add.', {
+              position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error('Post http status =' + res.status);
+          }
+        })
+        .catch((err) => {
+          // Display error toast notification
+		  if (err.message === '0'){
+			  console.err('Error: Network failure or CORS issue');
+		  }
+          toast.error('Error to add.', {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err);
+        });
+    };
+	
+	 render() {
+    return (
+      <div>
+        {/* Button to open the dialog */}
+        <Button variant="outlined" color="primary" style={{ margin: 10 }} onClick={this.handleClickOpen}>
+          Add New Student
+        </Button>
+        {/* Dialog for adding a student */}
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+		
+          <DialogTitle>Add New Student</DialogTitle>
+		  
+          <DialogContent style={{ paddingTop: 20 }}>
+            {/* Text field for entering the name */}
+            <TextField autoFocus fullWidth label="Name" name="name" onChange={this.handleChange} />
+          </DialogContent>
+		  
+          <DialogContent style={{ paddingTop: 20 }}>
+            {/* Text field for entering the email */}
+            <TextField autoFocus fullWidth label="E-mail" name="email" onChange={this.handleChange} />
+          </DialogContent>
+		  
+          <DialogActions>
+		  
+            {/* Button to cancel adding a student */}
+            <Button color="secondary" onClick={this.handleClose}>
+              Cancel
+            </Button>
+			
+            {/* Button to add a new student */}
+            <Button id="add" color="primary" onClick={this.handleAdd}>
+              Add New
+            </Button>
+			
+          </DialogActions>
+		  
+        </Dialog>
+        {/* Toast container for displaying notifications */}
+        <ToastContainer autoClose={1500} />
+      </div>
+    );
+  }
+}
+
+// required property:  function to call to perform the Add action
+AddStudent.propTypes = {
+  newStudent : PropTypes.func.isRequired
+}
+
+
+export default AddStudent;
--- src/components/SchedList.js
+++ src/components/SchedList.js
@@ -24,17 +24,24 @@ import AddCourse from './AddCourse';
 class SchedList extends Component {
   constructor(props) {
     super(props);
+	//initially empty list of courses go to backend	to get list student is registered for
+	//courses comes from ScheduleDTO List<CourseDTO> courses
     this.state = { courses: [] };
   } 
   
+  //when component 1st opens calls lifecycle method
   componentDidMount() {
     this.fetchCourses();
   }
   
+  //downloads data and put into state variable and update courses
   fetchCourses = () => {
     console.log("SchedList.fetchCourses");
     const token = Cookies.get('XSRF-TOKEN');
-    
+	
+    //properties year & semester required use props.location to access
+	//SERVER_URL is localhost 8080
+	//properties year and semester defined in semester.js in button component
     fetch(`${SERVER_URL}/schedule?year=${this.props.location.year}&semester=${this.props.location.semester}`, 
       {  
         method: 'GET', 
@@ -46,6 +53,7 @@ class SchedList extends Component {
     .then((responseData) => { 
       // do a sanity check on response
       if (Array.isArray(responseData.courses)) {
+		//change original state of courses list from constructor
         this.setState({ 
           courses: responseData.courses,
         });
@@ -60,14 +68,16 @@ class SchedList extends Component {
           position: toast.POSITION.BOTTOM_LEFT
         });
         console.error(err); 
-    })
+    })//error message to display
   }
 
   // Drop Course 
   onDelClick = (id) => {
+	//alerts with a popup window
     if (window.confirm('Are you sure you want to drop the course?')) {
       const token = Cookies.get('XSRF-TOKEN');
       
+	  //get id to delete in postman toast sends a message after action
       fetch(`${SERVER_URL}/schedule/${id}`,
         {
           method: 'DELETE',
@@ -90,14 +100,14 @@ class SchedList extends Component {
               position: toast.POSITION.BOTTOM_LEFT
         });
         console.error(err);
-      }) 
+      }) //error to delete
     } 
   }
 
   // Add course
   addCourse = (course) => {
     const token = Cookies.get('XSRF-TOKEN');
- 
+	//post in postman toast sends a message after action if ok or not failure msg
     fetch(`${SERVER_URL}/schedule`,
       { 
         method: 'POST', 
@@ -110,6 +120,7 @@ class SchedList extends Component {
           toast.success("Course successfully added", {
               position: toast.POSITION.BOTTOM_LEFT
           });
+		  //downloads new updated schedule from the backend
           this.fetchCourses();
         } else {
           toast.error("Error when adding", {
@@ -122,9 +133,13 @@ class SchedList extends Component {
             position: toast.POSITION.BOTTOM_LEFT
         });
         console.error(err);
-    })
+    })//error to post
   } 
-
+	
+  //table fields displayed
+  //displays cols in the DataGrid every row needs id col for each row
+  //column header in fields, state for editable, state.selected is index of row selected
+  //specify custom rendering
   render() {
      const columns = [
       { field: 'title', headerName: 'Title', width: 400 },
@@ -154,28 +169,41 @@ class SchedList extends Component {
   
   return(
       <div>
+		
           <AppBar position="static" color="default">
+		  
             <Toolbar>
+			   {/*type bar displays Schedule 2020 Fall for example*/}
                <Typography variant="h6" color="inherit">
                   { 'Schedule ' + this.props.location.year + ' ' +this.props.location.semester }
                 </Typography>
             </Toolbar>
+			
           </AppBar>
+		  
           <div className="App">
+		  
+		  {/*JSON is for debugging purposes to display raw data*/}
             <div style={{width:'100%'}}>
                 For DEBUG:  display state.
                 {JSON.stringify(this.state)}
             </div>
+			
             <Grid container>
               <Grid item>
+			  
+			  {/*button to add course from AddCourse.js render when clicked*/}
 			    <ButtonGroup>
                   <AddCourse addCourse={this.addCourse}  />
 				</ButtonGroup>
               </Grid>
             </Grid>
+			
+			{/*table to show list of enrolled courses to display and columns initiated above*/}
             <div style={{ height: 400, width: '100%' }}>
               <DataGrid rows={this.state.courses} columns={columns} />
             </div>
+			
             <ToastContainer autoClose={1500} />   
           </div>
       </div>
